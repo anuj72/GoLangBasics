16c8753967541030c8cffa089d26993c85331c64
Anuj Sachan <anujwebhost@gmail.com>
2021-07-10 23:20:12 +0530

    array

--- array.go
+++ array.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("hello")
+	var arr = []int{1, 2, 3, 4}
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	for index, element := range arr {
+		fmt.Println(index, element)
+	}
+
+	for _, element := range arr {
+		fmt.Println(element)
+	}
+
 }
