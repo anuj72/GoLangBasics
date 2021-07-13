f1d7829abcb7344ad326e58eaaafcf9fa8b38dba
Anuj Sachan <anujwebhost@gmail.com>
2021-07-13 11:49:53 +0530

    array

--- interface.go
+++ interface.go
@@ -1,14 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var t BookT
+	t = BookT{"10", "14", 11}
+	fmt.Println("Area of tank :", t.Tarea())
+
+}
+func (m BookT) Tarea() float64 {
+
+	return float64(2 * m.page)
 }
 
 type book interface {
 	sportsName() string
 }
 
-type Book struct {
+type BookT struct {
 	name   string
 	author string
 	page   int
